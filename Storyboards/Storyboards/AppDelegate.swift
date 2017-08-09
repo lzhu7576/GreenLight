0fa853523ae699107646ac01daea6d19504c098f
朱力 <30821973+lzhu7576@users.noreply.github.com>
2017-08-09 10:08:32 +0800

    Update AppDelegate.swift

--- Storyboards/Storyboards/AppDelegate.swift
+++ Storyboards/Storyboards/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        
+        //测试的注释
         return true
     }
 
